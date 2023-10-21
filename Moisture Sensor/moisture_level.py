171f26212445280736b9f4475fe94c1cae868a7f
Aravindhakumar M <65991985+Aravindhakumar-M@users.noreply.github.com>
2023-10-21 21:58:34 +0530

    Add files via upload

---
+++ Moisture Sensor/moisture_level.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int16
+import serial
+import time
+
+# Initialize a ROS node named 'moisture_node'
+rospy.init_node('moisture_node', anonymous=True)
+
+# Print a message to indicate that the moisture sensing node has started
+print("Started Moisture Sensing node")
+
+# Create a ROS publisher to publish moisture level data to the '/moisture_level' topic
+pub = rospy.Publisher('/moisture_level', Int16, queue_size=10)
+
+# Initialize a serial connection to the moisture sensor on '/dev/ttyUSB0' with a baud rate of 9600 and a timeout of 1 second
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1.00)
+
+# Wait for 1 second to ensure the serial connection is established
+time.sleep(1)
+
+# Reset the input buffer of the serial connection
+ser.reset_input_buffer()
+
+try:
+    while True:
+        # Pause the loop for 0.01 seconds to control the sensor data reading rate
+        time.sleep(0.01)
+
+        # Check if there is data waiting to be read from the serial connection
+        if ser.in_waiting > 0:
+            # Read a line of data from the serial connection and decode it as UTF-8
+            line = ser.readline().decode('utf-8')
+
+            # Publish the moisture level data as an Int16 message to the '/moisture_level' topic
+            pub.publish(Int16(int(line)))
+
+except KeyboardInterrupt:
+    # Close the serial connection gracefully when the script is terminated
+    ser.close()
