aeabfc0baddb0dd76f4bb005c2781344a2cc2da1
Aravindhakumar M <65991985+Aravindhakumar-M@users.noreply.github.com>
2023-10-21 22:17:44 +0530

    Add files via upload

---
+++ Prismatic Arm Controller/arm_controller.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int16
+import RPi.GPIO as GPIO
+import time
+
+flag = False  # Flag to control arm movement
+moisture = 0  # Moisture level
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)  # Control pin for one arm movement direction
+GPIO.setup(22, GPIO.OUT)  # Control pin for the other arm movement direction
+GPIO.setup(23, GPIO.OUT)  # Arm control signal
+GPIO.setup(21, GPIO.IN)   # Input pin for depth sensor
+
+GPIO.output(23, True)     # Enable the arm control signal
+GPIO.output(17, False)    # Set one arm direction to low
+GPIO.output(22, True)     # Set the other arm direction to high
+
+# Callback function to handle moisture level data
+def func_moisture(moist):
+    global moisture
+    moisture = moist.data
+
+# Callback function to control the arm based on depth sensor data
+def func_depth(data):
+    global flag
+
+    # Check if the depth reading is less than 5 (considering it as a water depth threshold)
+    if data.data < 5:
+
+        # If the depth sensor detects water and arm control signal is high
+        if GPIO.input(21) == 1:
+            GPIO.output(17, False)  # Turn off one arm direction
+            GPIO.output(22, False)  # Turn off the other arm direction
+            flag = True  # Set the flag to True
+
+        # If the depth sensor detects water and flag is False
+        elif flag == False:
+            GPIO.output(17, False)  # Turn off one arm direction
+            GPIO.output(22, True)   # Set the other arm direction to high
+
+        # If the depth sensor detects water and flag is True
+        elif flag == True:
+            GPIO.output(17, True)    # Set one arm direction to high
+            GPIO.output(22, False)   # Turn off the other arm direction
+
+            # Check the moisture level
+            if moisture > 20:
+                GPIO.output(17, False)  # Turn off one arm direction
+                GPIO.output(22, False)  # Turn off the other arm direction
+                time.sleep(2)           # Wait for 2 seconds
+
+                # Set the arm direction to retract
+                GPIO.output(17, False)
+                GPIO.output(22, True)
+
+# Callback function to handle shutdown
+def shutdown():
+    GPIO.cleanup()  # Cleanup GPIO pins
+
+# Main execution
+if __name__ == '__main__':
+    try:
+        rospy.init_node('arm_cont', anonymous=True)
+        print("Started Arm Controller node")
+
+        # Subscribe to depth and moisture level topics
+        rospy.Subscriber('/depth', Int16, func_depth)
+        rospy.Subscriber('/moisture_level', Int16, func_moisture)
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        pass
+    finally:
+        shutdown()  # Cleanup GPIO pins on node shutdown
