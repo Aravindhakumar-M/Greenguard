cd339de4ac3497429e195a2d4e44c0e89ceef767
Aravindhakumar M <65991985+Aravindhakumar-M@users.noreply.github.com>
2024-01-20 21:48:08 +0530

    Update pump_controller.py

--- Pump Controller/pump_controller.py
+++ Pump Controller/pump_controller.py
@@ -9,10 +9,7 @@ import time
 MOSFET_IN = 18
 MOSFET_OUT = 19
 
-# Set the GPIO mode to use board pin numbering
 gpio.setmode(gpio.BOARD)
-
-# Set up GPIO pins as output
 gpio.setup(MOSFET_IN, gpio.OUT)
 gpio.setup(MOSFET_OUT, gpio.OUT)
 
@@ -44,21 +41,17 @@ def outlet(moist):
         print("Outlet Pump Turned ON for 8 seconds")
         time.sleep(8)
 
-# Function to perform GPIO cleanup
 def shutdown():
     gpio.cleanup()
 
 if __name__ == '__main__':
     try:
-        # Initialize the ROS node
         rospy.init_node('pump_controller', anonymous=True)
         print("Started Pump Controller")
 
-        # Subscribe to topics for moisture level and water level
         rospy.Subscriber('/moisture_level', Int16, outlet)
         rospy.Subscriber('/water_level', Int16, inlet)
 
-        # Start the ROS node and listen for messages
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
